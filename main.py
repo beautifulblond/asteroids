202216ca7b54b1628ceb50b435a4abfa37655366
beautifulblond <ryanalholder@gmail.com>
2025-04-09 13:39:40 -0700

    fps set to 60

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from constants import *
 def main():
     pygame.init()
     
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     going = True
@@ -15,10 +22,10 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
     
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
 
 
 if __name__ == "__main__":
