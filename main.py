bdfee535bacbaf9372c8fa67e909082366110a99
beautifulblond <ryanalholder@gmail.com>
2025-04-09 15:53:07 -0700

    added Player class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 
@@ -15,12 +16,16 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    going = True
-    while going == True:
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # The following should remain as ordered    
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
