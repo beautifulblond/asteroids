d5a6d226ec9a86ae6e290cd7f71fa18e05c08f49
beautifulblond <ryanalholder@gmail.com>
2025-04-11 13:23:55 -0700

    created groups to be set as containers for drier code

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from constants import *
 from player import Player
 
 
-
 def main():
     pygame.init()
     
@@ -14,25 +13,41 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
+        dt = clock.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        #player.update(dt)
+        #Player class is now contained by updateable
+        
         
         # The following should remain as ordered    
         screen.fill("black")
-        player.draw(screen)
+        
+        updateable.update(dt)
+        
+        #player.draw(screen)
+        #Player class is now contained by drawable
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick(60) / 1000
+        #dt = clock.tick(60) / 1000
         
     
 
 
 if __name__ == "__main__":
     main()
+
+pygame.quit()
